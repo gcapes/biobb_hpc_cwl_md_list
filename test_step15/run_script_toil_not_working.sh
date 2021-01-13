243b53322f9cf52a5ad64fa75049ab2cb7df025d
Douglas Lowe <10961945+douglowe@users.noreply.github.com>
2021-01-13 18:12:42 +0000

    Deleting the test code for step15

--- test_step15/run_script_toil_not_working.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# ensure that cwl conda environment is loaded
-#
-# conda activate toilcwl
-
-rm -rf lys2.pdb
-
-toil-cwl-runner --enable-dev --noMoveExports md_step15_only_toil.cwl md_step15_input_descriptions.yml 
