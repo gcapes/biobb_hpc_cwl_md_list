339c81088139db9f73c77cbbb79c4b5ecfaa1896
Gerard Capes <gerard.capes@manchester.ac.uk>
2022-03-08 16:16:12 +0000

    Export variables for running toil

--- csf3_sge_running_script.sh
+++ csf3_sge_running_script.sh
@@ -6,25 +6,28 @@
 # and that the following environment variables are set and exported (or set in this script).
 #
 # TMPDIR - this needs to be one a shared disk accessible by all compute and login nodes
-# TOIL_GRIDENGINE_ARGS - this will contain SGE settings for the compute jobs, e.g.:
-# TOIL_GRIDENGINE_ARGS=
-# TOIL_GRIDENGINE_PE - this will give the parallel environment for running jobs, e.g.:
-# TOIL_GRIDENGINE_PE=smp.pe
-# CWL_SINGULARITY_CACHE - this is the download location for singularity, which should be on a shared disk  
-# SINGULARITY_CACHEDIR - this is the singularity working directory, which should be on a shared disk
+# Set SGE arguments for the compute jobs
+export TOIL_GRIDENGINE_ARGS=
+# Set the parallel environment for running jobs:
+export TOIL_GRIDENGINE_PE=smp.pe
+# Set the download location for singularity, which should be on a shared disk.
+export CWL_SINGULARITY_CACHE=~/scratch/bioexcel/docker_store
+# Set the singularity working directory, which should be on a shared disk.
+export SINGULARITY_CACHEDIR=~/scratch/bioexcel/.singularity
+mkdir -p $CWL_SINGULARITY_CACHE
 
 # Flags for running SGE and Toil. Note that the defaultCores sets the usage for the jobs,
 # which should fit within the nodes selected information in TOIL_SLURM_ARGS.
 SGE_FLAGS="--batchSystem grid_engine --defaultCores 6"
 
 # Flags for general Toil usage - on HPC we generally need to disable caching and use singularity.
-# Create the workdir directory beforehand.
-TOIL_FLAGS="--workDir ./workdir --disableCaching --singularity"
+TOIL_FLAGS="--disableCaching --singularity"
 
 # Flags for gathering data for stats analysis (these are stored in the cache directory)
 # Create the jobstore base directory beforehand, and adapt the naming convention for the
 # individual jobstore directories as you need.  
-STAT_FLAGS="--stats --jobStore ./jobstore/test_$(date "+%Y-%m-%d_%H-%M")"
+JOBSTORE=jobstore
+STAT_FLAGS="--stats --jobStore $JOBSTORE"
 
 toil-cwl-runner $TOIL_FLAGS $STAT_FLAGS $SGE_FLAGS md_list.cwl md_list_input_descriptions.yml
 
